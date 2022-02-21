42eb7367c0567d4fc4ddbdf3896217815c841971
Alejandro A <a.ahogado@uniandes.edu.co>
2022-02-20 22:17:21 -0500

    Avances con posibles errores

--- src/Buzon.java
+++ src/Buzon.java
@@ -8,14 +8,14 @@ public class Buzon {
 	private int tamano;
 	private char id;
 	private int contador;
-	private static Productor thr;
+	//private static Productor thr;
 	
 	public Buzon(int tamano, char id) {
 		this.tamano = tamano;
 		this.mensajes = new LinkedList<String>();
 		this.id = id;
 		this.contador = 0;
-		this.thr = thr;
+		//this.thr = thr;
 	}
 	
 	public synchronized boolean hayMensajes() {
@@ -23,9 +23,14 @@ public class Buzon {
 	}
 	
 	public synchronized void insertarMensaje(String mensaje) {
-		while(this.mensajes.size()==this.tamano) {
+		
+//IMPORTANTE!!		
+//revisar el while con el tamano
+		
+		while(this.mensajes.size()<this.tamano) {
 			try {
 				wait();
+				//falta espera activa4
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
@@ -42,16 +47,16 @@ public class Buzon {
 	public synchronized String retirarMensaje() {
 		while(this.mensajes.size()==0) {
 
-			if (thr.recibioActivo == false){
+			//if (thr.recibioActivo == false){
 				try {
 					wait();
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
-			}
-			else{
-				thr.yield();
-			}
+			//}
+//			else{
+//				thr.yield();
+//			}
 		}
 		
 		String mensaje = this.mensajes.remove(0);
--- src/Productor.java
+++ src/Productor.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Scanner;
 
 public class Productor extends Thread {
 	
@@ -10,23 +11,30 @@ public class Productor extends Thread {
 	private boolean transmitioActivo;
 	private String mensajeCopia;
 	
+	//contador para saber veces que entrega el 1
+	private int contadorUno;
+	
 	public Productor(int id,  ArrayList<Buzon> buzones, int times) {
 		this.id = id;
 		this.buzones = buzones;
 		this.times = times;	
 	}
 	
-	private void enviarMensajes(int i, char buzonEnviar, String mensaje) {
+	public void enviarMensajes(String mensaje, int cantidadMensajes) {
 		
 		mensajeCopia = mensaje;
+		contadorUno = 0;
 		
 		if (!mensaje.equals("FIN")) {
 			if (this.id==1) {
-				this.buzones.get(0).insertarMensaje(
-						mensajeCopia + " Identificador del thread: " +this.id +
-						" Lo recibio de forma activa? "+ this.recibioActivo +
-						" Lo transmitio de forma activa? "+ this.transmitioActivo 
-						);
+				if(cantidadMensajes>=contadorUno) {	
+						this.buzones.get(0).insertarMensaje(
+								mensajeCopia + " Identificador del thread: " +this.id +
+								" Lo recibio de forma activa? "+ this.recibioActivo +
+								" Lo transmitio de forma activa? "+ this.transmitioActivo 
+								);
+						contadorUno++;
+				}else {System.out.println(mensajeCopia);}
 			}
 			if (this.id==2) {
 				this.buzones.get(1).insertarMensaje(
@@ -67,7 +75,7 @@ public class Productor extends Thread {
 		
 	}
 	
-	private void sacarMensajes(int i, char buzonSacar) {
+	public void sacarMensajes() {
 		
 		if (this.id==1) {
 			mensajeCopia = this.buzones.get(3).retirarMensaje();
@@ -84,6 +92,76 @@ public class Productor extends Thread {
 		}		
 		
 	}
+	
+	
+	
+	
+	//Parte que ejecuta
+	
+	@Override
+	public void run() {
+		
+		
+//		while (contadorIM<1 && this.id==1) {
+//			for (int i = 0; i < array.length; i++) {
+//				
+//			}
+//			enviarMensajes(,cantiMensajes);	
+//			
+//			contadorIM++;
+//		}
+		
+		enviarMensajes("Hola", cantiMensajes);
+		sacarMensajes();
+	}
+	
+	
+	public static int cantiMensajes;
+	public int contadorIM = 0;
+	public String mensajes[] = new String[0];
+	
+	
+	public static void main(String[] args) {
+		
+		
+		Scanner teclado = new Scanner(System.in);
+		int contador = 0;
+
+		System.out.println("Cuantos mensajes van a ingresar?");
+		cantiMensajes = teclado.nextInt();
+		String mensajes[] = new String [cantiMensajes];
+
+		while(contador<cantiMensajes){
+			System.out.println("Digite su mensaje: ");
+			mensajes[contador] = teclado.nextLine();
+			contador++;
+		}
+		
+		Buzon b1 = new Buzon(4, 'A');
+		Buzon b2 = new Buzon(6, 'B');
+		Buzon b3 = new Buzon(2, 'C');
+		Buzon b4 = new Buzon(3, 'D');
+		
+		ArrayList<Buzon> buzones = new 	ArrayList();
+		
+		buzones.add(b1);
+		buzones.add(b2);
+		buzones.add(b3);
+		buzones.add(b4);
+		
+		Productor p1 = new Productor(1, buzones, 0);
+		Productor p2 = new Productor(2, buzones, 0);
+		Productor p3 = new Productor(3, buzones, 0);
+		Productor p4 = new Productor(4, buzones, 0);
+		
+		System.out.println("antes del start");
+		p1.start();
+		p2.start();
+		p3.start();
+		p4.start();
+	}
+	
+	
 	
 	
 	
